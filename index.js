44fde0a8313e45f4e779c333dcafdba989816b81
Anne Nabwire <nabanne96@gmail.com>
2025-01-16 14:54:05 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+  const main = document.getElementById("main");
+main.remove();
+const body1 =document.querySelector('body');
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.textContent='Anne Nabwire is the champion';
+body1.append(newHeader);
